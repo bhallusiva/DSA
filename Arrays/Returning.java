df9e954727360f4b0f9c03703ba706f2948c46f2
bhallusiva <bhallusivakumar@gmail.com>
2026-02-28 18:18:50 +0530

    Brute force Aproach for Two Sum

---
+++ Arrays/Returning.java
@@ -0,0 +1,30 @@
+package Arrays;
+import java.util.*;
+public class Returning {
+    int[] array_return(int arr[],int target)
+    {
+        int n=arr.length;
+        int comparsions=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]+arr[j]==target)
+                {
+                    comparsions++;
+                    return new int[] {i,j}; 
+                }
+            }
+        }
+        return new int[]{-1,-1};
+        
+    }
+public static void main(String args[])
+{
+    Returning ob=new Returning();
+    int arr[]={10,20,30};
+    int target=40;
+    System.out.println(Arrays.toString(ob.array_return(arr,target)));
+}    
+}
+
