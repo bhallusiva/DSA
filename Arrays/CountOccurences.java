81d81b0c1f94fc158613b7147da11792c460941a
bhallusiva <bhallusivakumar@gmail.com>
2026-03-01 07:46:51 +0530

    counting the occurences with HashMap , TC - O(n)

---
+++ Arrays/CountOccurences.java
@@ -0,0 +1,23 @@
+package DSA.Arrays;
+
+import java.util.HashMap;
+
+public class CountOccurences {
+    public static void main(String args[])
+    {
+        int[] arr={10,20,10,30,20};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int ele:arr)
+        {
+            int count=1;    
+            if(map.containsKey(ele))
+            {
+                count=map.get(ele);
+                map.put(ele, count+1);
+            }else{
+                map.put(ele,count);
+            }
+        }
+        System.out.println(map);
+    }
+}
